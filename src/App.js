98c76d3768a44cc4b3d943d9580f28d29e1c621d
Carlos <carlosvenegass8c@gmail.com>
2024-01-17 20:12:33 -0300

    subiendo calculadora finalizada

--- src/App.js
+++ src/App.js
@@ -12,39 +12,8 @@ function App() {
   const symbols = ['+', '-', '*', '/', '+/-','%']
 
   const modifyOperation = (value) => {
-    if (value === '%' && operation !== '') {
-      try {
-        const parts = operation.split(/[\+\-\*\/]/);
-        const lastPart = parts.pop();
-        const number = parseFloat(lastPart.replace('%', ''));
-        const newNumber = number / 100
-        const result = (Number(parts.join('')) + newNumber) ;
   
-        setTotal(result);
-        setOperation(result);
-      } catch (error) {
-        setTotal('Error');
-      }
-      return;
-    }
-    if (value === '+/-' && operation == ''){
-      try {
-        const parts = operation.split('+/-');
-        const lastPart = parts.pop();
-        const number = (lastPart.replace('+/-', ''));
-        const newNumber = number * (-1);
-        console.log(parts)
-
-        setTotal(newNumber)
-        setOperation(newNumber)
-        console.log(setTotal(newNumber));
-      } catch (error){
-        setTotal('error')
-      }
-    }
-
-  
-    if (['+', '-', '*', '/', '%', '+/-'].includes(value) && symbols.find(sym => operation.includes(sym))) {
+    if (['+', '-', '*', '/'].includes(value) && symbols.find(sym => operation.includes(sym))) {
       const lastCharacter = operation[operation.length - 1];
     
       if (symbols.includes(lastCharacter)) {
@@ -66,15 +35,19 @@ function App() {
       return;
     }
     
-  
+    if (operation === '=') return setTotal(eval(operation));
+
     if (operation === '0') {
       return setOperation(value);
     }
   
     setOperation(operation + value);
+
   };
   
-  
+  const calculate = () => {
+    setTotal(eval(operation));
+  }
 
   const deleteValue = () => {
     if (operation.length === 1) return setOperation('0')
@@ -87,34 +60,37 @@ function App() {
     setTotal('0')
   }
 
-  const calculate = () => {
 
-      setTotal(eval(operation))
-  }
   return (
     <main >
       <span className="resultado" type="text">{total}</span>
-      <span className="displey" type="text">{operation}</span>
-      <Button className="especial" value="" onClick={reset} >C</Button>
-      <Button className="especial" value="/100" onClick={() => modifyOperation("%")}>%</Button>
-      <Button className="especial" value="/" onClick={() => modifyOperation("+/-")}>+/-</Button>
-      <Button className="operacion" value="/" onClick={() => modifyOperation("/")}><CgMathDivide /></Button>
-      <Button className="numero" value="7" onClick={() => modifyOperation("7")}>7</Button>
-      <Button className="numero" value="8" onClick={() => modifyOperation("8")}>8</Button>
-      <Button className="numero" value="9" onClick={() => modifyOperation("9")}>9</Button>
-      <Button className="operacion" value="*" onClick={() => modifyOperation("*")}>*</Button>
-      <Button className="numero" value="4" onClick={() => modifyOperation("4")}>4</Button>
-      <Button className="numero" value="5" onClick={() => modifyOperation("5")}>5</Button>
-      <Button className="numero" value="6" onClick={() => modifyOperation("6")}>6</Button>
-      <Button className="operacion" value="+" onClick={() => modifyOperation("+")}>+</Button>
-      <Button className="numero" value="1" onClick={() => modifyOperation("1")}>1</Button>
-      <Button className="numero" value="2" onClick={() => modifyOperation("2")}>2</Button>
-      <Button className="numero" value="3" onClick={() => modifyOperation("3")}>3</Button>
-      <Button className="operacion" value="-" onClick={() => modifyOperation("-")}>-</Button>
-      <Button className="numero" value="0" onClick={() => modifyOperation("0")}>0</Button>
-      <Button className="numero" value="." onClick={() => modifyOperation(".")}>.</Button>
-      <Button className="especial" value="" onClick={deleteValue}><LuDelete /></Button>
-      <Button className="operacion" value="=" onClick={calculate}>=</Button>
+      <span className="display" type="text">{operation}</span>
+      <div className='container'>
+        <Button className="operacion" value="+" onClick={() => modifyOperation("+")}>+</Button>
+        <Button className="operacion" value="*" onClick={() => modifyOperation("*")}>*</Button>
+        <Button className="operacion" value="-" onClick={() => modifyOperation("-")}>-</Button>
+        <Button className="operacion" value="/" onClick={() => modifyOperation("/")}><CgMathDivide /></Button>
+      </div>
+      <div className="body">
+      <div className='numbers'>
+        <Button className="numero" value="7" onClick={() => modifyOperation("7")}>7</Button>
+        <Button className="numero" value="8" onClick={() => modifyOperation("8")}>8</Button>
+        <Button className="numero" value="9" onClick={() => modifyOperation("9")}>9</Button>
+        <Button className="numero" value="4" onClick={() => modifyOperation("4")}>4</Button>
+        <Button className="numero" value="5" onClick={() => modifyOperation("5")}>5</Button>
+        <Button className="numero" value="6" onClick={() => modifyOperation("6")}>6</Button>     
+        <Button className="numero" value="1" onClick={() => modifyOperation("1")}>1</Button>
+        <Button className="numero" value="2" onClick={() => modifyOperation("2")}>2</Button>
+        <Button className="numero" value="3" onClick={() => modifyOperation("3")}>3</Button>
+        <Button className="numero" value="0" onClick={() => modifyOperation("0")}>0</Button>
+        <Button className="numero" value="." onClick={() => modifyOperation(".")}>.</Button>
+        <Button className="especial" value="" onClick={deleteValue}><LuDelete /></Button>
+      </div>
+      <div className='buttons'>
+        <Button className="especial" value="" onClick={reset} >C</Button>
+        <Button className="especial" value="=" onClick={calculate}>=</Button>
+      </div>
+      </div>
 
     </main>
   );
